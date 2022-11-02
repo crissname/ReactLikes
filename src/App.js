75c14e54a67d6deeddb4ae03bf8e89bdc458a800
Artyom <crissname@gmail.com>
2022-11-02 22:48:30 +0200

    start logick

--- src/App.js
+++ src/App.js
@@ -1,57 +1,44 @@
 import './index.css';
+import './question';
+import {useState} from "react";
+import {questions} from "./question";
 
-const questions = [
-  {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
-];
 
 function Result() {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>
       <h2>Вы отгадали 3 ответа из 10</h2>
       <button>Попробовать снова</button>
     </div>
   );
 }
 
-function Game() {
+function Game({question, onClickVariant}) {
   return (
     <>
       <div className="progress">
-        <div style={{ width: '20%' }} className="progress__inner"></div>
+        <div style={{width: '20%'}} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((text, index) => (
+          <li onClick={()=> onClickVariant(index)} key={index}>{text}</li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, useStep] = useState(0);
+  const question = questions[step];
+  const onClickVariant = (index) => {
+    console.log(step, index);
+  }
   return (
     <div className="App">
-      <Game />
+      <Game question={question} onClickVariant={onClickVariant}/>
       {/* <Result /> */}
     </div>
   );
